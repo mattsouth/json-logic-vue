8d77321f2a5ab609c80785eda4fe5de89ccc02bf
Matt South <matt@mattsouth.net>
2021-10-24 17:57:09 +0100

    first pass at showing "truth table" style evaluations

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,50 @@
 <template>
-<div class="col-lg-8 mx-auto p-3 py-md-5">
+<div class="col-lg-12 mx-auto p-3">
     <header>
-        <span class="fs-4">json-logic</span>
+        <span class="fs-2">json-logic demo</span>
     </header>
     <main>
         <div class="row">
             <div class="col">
-                Data
-                <json-editor :value="context" @update="context = $event" />
+                <div class="fs-4">Context</div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>name</th>
+                            <th>type</th>
+                            <th>value(s)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="dd in context" v-bind:key="dd">
+                            <td>{{dd.name}}</td>
+                            <td>{{dd.type}}</td>
+                            <td>{{renderValues(dd.values)}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div class="col">
-                Expression
-                <json-editor :value="expr" @update="expr = $event" />
+                <div class="fs-4">Expression</div>
                 <input type="text" v-model="render" />
+                <json-editor :value="expr" @update="expr = $event" />
             </div>
             <div class="col">
-                Result
-                <input type="text" :value="JSON.stringify(result)" />
+                <div class="fs-4">Evaluation</div>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th v-for="name in dds" v-bind:key="name">{{name}}</th>
+                            <th>value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="res in result" v-bind:key="res">
+                            <td v-for="name in dds" v-bind:key="name">{{renderValue(res[name])}}</td>
+                            <td>{{renderValue(res._value)}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </main>
@@ -33,8 +61,11 @@ export default {
     name: 'App',
     data() {
         return {
-            context: { age: 56 },
-            expr: { ">=": [{var: "age"}, 18]}
+            context: [
+                {name: "age", type: "integer", values: [null, -Infinity, 0, 17, 18, 19, Infinity]},
+                {name: "presentable", type: "boolean", values: [null, false, true]}
+            ],
+            expr: { "and": [{">=": [{var: "age"}, 18]}, {var: "presentable"}]}
         }
     },
     components: {
@@ -42,17 +73,50 @@ export default {
     },
     computed: {
         result() {
-            return jsonLogic.apply(this.expr, this.context);
+            const r = [];
+            const context = this.context;
+            const dds = this.dds;
+            function helper(obj, i) {
+                for (var j=0, l=context[i].values.length; j<l; j++) {
+                    var o = { ...obj }; // clone obj
+                    o[dds[i]]= context[i].values[j];
+                    if (i==(context.length-1))
+                        r.push(o);
+                    else
+                        helper(o, i+1);
+                }
+            }
+            // form an array of possible combination values of context.
+            helper({}, 0);
+            // evaluate expression values for that array
+            for (let comb of r) {
+                let val = jsonLogic.apply(this.expr, comb);
+                comb._value = val;
+            }
+            return r;
+        },
+        dds() {
+            return this.context.map((val) => val.name);
         },
         render: {
             get: function() {
                 return renderJsonLogic(this.expr);
             },
             set: function(text) {
-                // console.log('render:set', text);
-                // console.log(JSON.stringify(transformJS(text)));
                 this.expr = transformJS(text);
             }
+        }
+    },
+    methods: {
+        renderValue(val) {
+            if (val == null) {
+                return "null";
+            } else {
+                return val.toString();
+            }
+        },
+        renderValues(vals) {
+            return vals.map((val) => this.renderValue(val)).join(", ");
         }
     }
 }
