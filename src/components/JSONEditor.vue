77bf493b300f25b602635ca1aaa0de059f3b862d
Matt South <matt@mattsouth.net>
2021-10-26 22:02:11 +0100

    tidied up json editor border
    and formatted the code

--- src/components/JSONEditor.vue
+++ src/components/JSONEditor.vue
@@ -1,59 +1,67 @@
+<docs>
+A version of the ace-editor that edits a Json Logic object.
+</docs>
+
 <template>
-    <v-ace-editor
-        v-model:value="text"
-        lang="json"
-        theme="textmate"
-        :options="{tabSize: 2, useWorker: true}"
-        :style="style" />
+  <v-ace-editor
+    v-model:value="text"
+    lang="json"
+    theme="textmate"
+    :options="{ tabSize: 2, useWorker: true }"
+    style="height: 300px; border-radius: .25rem; border: 1px solid #ced4da;"
+  />
 </template>
 
 <script>
-import { VAceEditor } from 'vue3-ace-editor';
-import ace from 'ace-builds';
-import 'ace-builds/src-noconflict/mode-json';
-import 'ace-builds/src-noconflict/theme-textmate';
-import workerJsonUrl from 'file-loader?esModule=false!ace-builds/src-noconflict/worker-json.js';
+import { VAceEditor } from "vue3-ace-editor";
+import ace from "ace-builds";
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-textmate";
+import workerJsonUrl from "file-loader?esModule=false!ace-builds/src-noconflict/worker-json.js";
 
-ace.config.setModuleUrl('ace/mode/json_worker', workerJsonUrl);
+ace.config.setModuleUrl("ace/mode/json_worker", workerJsonUrl);
 
 export default {
-    name: 'json-editor',
-    props: {
-        value: Object
-    },
-    data: function() {
-      return {
-        jsonValid: true
-      };
-    },
-    components: {
-        VAceEditor,
-    },
-    computed: {
-        text: {
-            // JSON text for atom editor
-            get: function() {
-              return JSON.stringify(this.value,null,2);
-            },
-            set: function(text) {
-              if (text.length>0) {
-                let json;
-                try {
-                  if (text!==this.text) {
-                    json = JSON.parse(text);
-                    this.jsonValid = true;
-                    this.$emit('update', json);
-                  }
-                } catch(e) {
-                  // assumes exception caused by hand editing the JSON
-                  this.jsonValid = false;
-                }
-              }
+  props: {
+    value: Object,
+  },
+  data: function () {
+    return {
+      jsonValid: true,
+    };
+  },
+  components: {
+    VAceEditor,
+  },
+  computed: {
+    text: {
+      // JSON text for atom editor
+      get: function () {
+        return JSON.stringify(this.value, null, 2);
+      },
+      set: function (text) {
+        if (text.length > 0) {
+          let json;
+          try {
+            if (text !== this.text) {
+              json = JSON.parse(text);
+              this.jsonValid = true;
+              this.$emit("update", json);
             }
-        },
-        style() {
-            return "height: 300px; border: " + (this.jsonValid ? "grey" : "red") + " 1px dashed";
+          } catch (e) {
+            // assumes exception caused by hand editing the JSON
+            this.jsonValid = false;
+          }
         }
-    }
-}
+      },
+    },
+    style() {
+      return (
+        "height: 300px; border: " +
+        (this.jsonValid ? "grey" : "red") +
+        " 1px dashed"
+      );
+    },
+  },
+};
 </script>
