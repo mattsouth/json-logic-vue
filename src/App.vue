af8ef5538992963c5a26b1d93685b61d7e50466d
Matt South <matt@mattsouth.net>
2021-10-26 13:32:21 +0100

    create context-table component

--- src/App.vue
+++ src/App.vue
@@ -14,20 +14,7 @@
             </div>
             <div class="col">
                 <div class="fs-4">Context</div>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>variable</th>
-                            <th>value(s)</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="dd in context" v-bind:key="dd" :class="{'text-muted text-decoration-line-through': !(variables.includes(dd.name))}">
-                            <td @click="openModal(dd)">{{dd.name}}</td>
-                            <td><span @click="openModal(dd)">{{renderValues(dd.values)}}</span><button v-if="!(variables.includes(dd.name))" type="button" @click="removeVar(dd)" class="btn btn-light btn-sm float-end">&times;</button></td>
-                        </tr>
-                    </tbody>
-                </table>
+                <context-table :value="context" :variables="variables" @update="context = $event" />
             </div>
             <div class="col">
                 <div class="fs-4">Evaluation</div>
@@ -48,41 +35,14 @@
             </div>
         </div>
     </main>
-    <div class="modal" tabindex="-1" ref="variableModal">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Edit variable: {{current.name}}</h5>
-          </div>
-          <div class="modal-body">
-            <table class="table">
-                <tbody>
-                    <tr v-for="(value, idx) in current.values" v-bind:key="idx" :class="{'table-active': (idx==modalState.valueIdx)}">
-                        <td>
-                            <span @click="selectContextValue(idx)">{{renderValue(current.values[idx])}}</span>
-                            <button v-if="value!=null" type="button" @click="removeValue(idx)" class="btn btn-light btn-sm float-end">&times;</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <input type="text" :value="contextValue" v-on:keyup.enter="updateContextValue" class="my-2 form-control"/>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          </div>
-        </div>
-      </div>
-    </div>
 </div>
 </template>
 
 <script>
 import JSONEditor from './components/JSONEditor';
 import TextExprEditor from './components/TextExprEditor';
+import ContextTable from './components/ContextTable';
 import * as jsonLogic from 'json-logic-js';
-import { Modal } from 'bootstrap';
-
-let varModal = null;
 
 export default {
     name: 'App',
@@ -92,20 +52,13 @@ export default {
                 {name: "age", values: [null, -Infinity, 0, 17, 18, 19, Infinity]},
                 {name: "presentable", values: [null, false, true]}
             ],
-            current: -1,
-            modalState: {
-                variableIdx: null,
-                valueIdx: -1
-            },
             expr: { "and": [{">=": [{var: "age"}, 18]}, {var: "presentable"}]}
         }
     },
-    mounted() {
-        varModal =  new Modal(this.$refs.variableModal);
-    },
     components: {
         'json-editor': JSONEditor,
-        'text-editor': TextExprEditor
+        'text-editor': TextExprEditor,
+        'context-table': ContextTable
     },
     computed: {
         result() {
@@ -163,13 +116,6 @@ export default {
         filtered() {
             return this.context.filter((val) => this.variables.includes(val.name));
         },
-        contextValue() {
-            if (this.modalState.variableIdx>-1 && this.modalState.valueIdx>-1) {
-                return this.renderValue(this.context[this.modalState.variableIdx].values[this.modalState.valueIdx]);
-            } else {
-                return null;
-            }
-        }
     },
     methods: {
         renderValue(val) {
@@ -182,42 +128,13 @@ export default {
         renderValues(vals) {
             return vals.map((val) => this.renderValue(val)).join(", ");
         },
-        removeVar(dd) {
-            this.context.splice(this.context.indexOf(dd),1);
-        },
-        openModal(dd) {
-            this.current = dd;
-            this.modalState = {
-                variableIdx: this.context.indexOf(dd),
-                valueIdx: -1
-            };
-            varModal.show();
-        },
-        selectContextValue(idx) {
-            if (this.modalState.valueIdx == idx) {
-                this.modalState.valueIdx=-1;
-            } else {
-                this.modalState.valueIdx=idx;
-            }
-        },
-        updateContextValue(evt) {
-            if (this.modalState.valueIdx == -1) {
-                this.context[this.modalState.variableIdx].values.push(JSON.parse(evt.target.value));
-            } else {
-                this.context[this.modalState.variableIdx].values[this.modalState.valueIdx].push(JSON.parse(evt.target.value));
-            }
-            this.modalState.valueIdx = -1;
-        },
-        removeValue(idx) {
-            this.context[this.modalState.variableIdx].values.splice(idx, 1);
-        }
-     },
+    },
     watch: {
         expr() {
             for (const dd of this.variables.filter(x => !this.dds.includes(x))) {
                 this.context.push({name: dd, values: [null]});
             }
-        }
+        },
     }
 }
 </script>
---
+++ src/components/ContextTable.vue
@@ -0,0 +1,175 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>variable</th>
+        <th>value(s)</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="dd in value"
+        v-bind:key="dd"
+        :class="{
+          'text-muted text-decoration-line-through': !variables.includes(
+            dd.name
+          ),
+        }"
+      >
+        <td @click="openModal(dd)">{{ dd.name }}</td>
+        <td>
+          <span @click="openModal(dd)">{{ renderValues(dd.values) }}</span
+          ><button
+            v-if="!variables.includes(dd.name)"
+            type="button"
+            @click="removeVar(dd)"
+            class="btn btn-light btn-sm float-end"
+          >
+            &times;
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <div class="modal" tabindex="-1" ref="variableModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Edit variable: {{ current.name }}</h5>
+        </div>
+        <div class="modal-body">
+          <table class="table">
+            <tbody>
+              <tr
+                v-for="(value, idx) in current.values"
+                v-bind:key="idx"
+                :class="{ 'table-active': idx == modalState.valueIdx }"
+              >
+                <td>
+                  <span @click="selectContextValue(idx)">{{
+                    renderValue(current.values[idx])
+                  }}</span>
+                  <button
+                    v-if="value != null"
+                    type="button"
+                    @click="removeValue(idx)"
+                    class="btn btn-light btn-sm float-end"
+                  >
+                    &times;
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <input
+            type="text"
+            :value="contextValue"
+            v-on:keyup.enter="updateContextValue"
+            class="my-2 form-control"
+          />
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Modal } from "bootstrap";
+
+let varModal = null;
+
+export default {
+  props: {
+    value: {
+      type: Array,
+      required: true,
+    },
+    variables: {
+      type: Array,
+      required: true,
+    },
+  },
+  emits: ['update'],
+  mounted() {
+    varModal = new Modal(this.$refs.variableModal);
+  },
+  data() {
+    return {
+      current: -1,
+      modalState: {
+        variableIdx: null,
+        valueIdx: -1,
+      },
+    };
+  },
+  computed: {
+    contextValue() {
+      if (this.modalState.variableIdx > -1 && this.modalState.valueIdx > -1) {
+        return this.renderValue(
+          this.value[this.modalState.variableIdx].values[
+            this.modalState.valueIdx
+          ]
+        );
+      } else {
+        return null;
+      }
+    },
+  },
+  methods: {
+    renderValue(val) {
+      if (val == null) {
+        return "null";
+      } else {
+        return val.toString();
+      }
+    },
+    renderValues(vals) {
+      return vals.map((val) => this.renderValue(val)).join(", ");
+    },
+    removeVar(dd) {
+      const clone = [...this.value];
+      clone.splice(this.value.indexOf(dd),1);
+      this.$emit("update", clone);
+    },
+    openModal(dd) {
+      this.current = dd;
+      this.modalState = {
+        variableIdx: this.value.indexOf(dd),
+        valueIdx: -1,
+      };
+      varModal.show();
+    },
+    selectContextValue(idx) {
+      if (this.modalState.valueIdx == idx) {
+        this.modalState.valueIdx = -1;
+      } else {
+        this.modalState.valueIdx = idx;
+      }
+    },
+    updateContextValue(evt) {
+      const clone =  [...this.value];
+      if (this.modalState.valueIdx == -1) {
+        clone[this.modalState.variableIdx].values.push(JSON.parse(evt.target.value));
+      } else {
+        clone[this.modalState.variableIdx].values[this.modalState.valueIdx].push(JSON.parse(evt.target.value));
+      }
+      this.$emit("update", clone);
+      this.modalState.valueIdx = -1;
+    },
+    removeValue(idx) {
+      const clone =  [...this.value];
+      clone[this.modalState.variableIdx].values.splice(idx, 1);
+      this.$emit("update", clone);
+    },
+  },
+};
+</script>
