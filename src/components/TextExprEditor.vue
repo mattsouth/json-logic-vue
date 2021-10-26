b4d76a3ea890b5391f5b4532d7d07a582b2ac628
Matt South <matt@mattsouth.net>
2021-10-26 22:01:43 +0100

    debounced text editing

--- src/components/TextExprEditor.vue
+++ src/components/TextExprEditor.vue
@@ -1,39 +1,44 @@
 <docs>
 A text input component that reflects and can edit a Javascript serialisation of a Json Logic expression.
 
-TODO: Debounce updates to avoid too much chatter 
+Updates are debounced to avoid too much chatter.
 TODO: Expose an error state if the textbox value cannot be deserialised
 TODO: Use a slot to pass styling classes from consumer
 </docs>
 
 <template>
-  <input type="text" v-model="render" class="my-2 form-control"/>
+  <input type="text" v-model="render" class="my-2 form-control" />
 </template>
 
 <script>
-import renderJsonLogic from 'json-logic-to-js';
-import transformJS from 'js-to-json-logic';
+import renderJsonLogic from "json-logic-to-js";
+import transformJS from "js-to-json-logic";
+
+let timeout;
 
 export default {
   props: {
     value: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
   computed: {
     render: {
-      get: function() {
+      get: function () {
         return renderJsonLogic(this.value);
       },
-      set: function(text) {
-        try {
-          this.$emit('update', transformJS(text));
-        } catch (e) {
-          console.log('error converting JL to JS');
-        }
-      }
-    },    
-  }
-}
+      set: function (text) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          try {
+            this.$emit("update", transformJS(text));
+          } catch (e) {
+            console.log(text, e);
+          }
+        }, 400);
+      },
+    },
+  },
+};
 </script>
